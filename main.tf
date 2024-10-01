52074f49038ed9ac4672421bcb5481bffeddca17
Ivica <ivica-matic@outlook.com>
2024-10-01 22:25:04 +0100

    fix: locked provider versions

--- main.tf
+++ main.tf
@@ -2,22 +2,22 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.3.0"
+      version = "4.3.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~>3.0.1"
+      version = "3.0.1"
     }
     acme = {
       source  = "vancluever/acme"
-      version = "~> 2.18.0"
+      version = "2.26.0"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 4.0"
+      version = "4.43.0"
     }
   }
-  required_version = ">= 1.1.0"
+  required_version = "1.5.5"
   backend "azurerm" {
     # will get configured with -backend-config= in the terraform init -upgrade command
     # the backend config file will be created using envsubst and provider_secrets_template file
