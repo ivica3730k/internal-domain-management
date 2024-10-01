f44b05ca6923e6ab2d1d80af81f82dc732428f98
Ivica <ivica-matic@outlook.com>
2024-10-01 22:42:20 +0100

    fix: fixed local setup script

--- local_setup.sh
+++ local_setup.sh
@@ -3,4 +3,4 @@ set -a;
 source .env;
 set +a;
 envsubst < provider_secrets_template > provider_secrets
-terraform init -backend-config=provider_secrets -reconfigure
+terraform init -backend-config=provider_secrets -reconfigure -upgrade
