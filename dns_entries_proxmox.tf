dd8d2bdbc1b95bd2277f8a767ec3946ea4c278bb
Ivica Matic <ivica.matic@groupnexus.co.uk>
2024-10-04 14:19:50 +0100

    feat: added dns entries for local proxmox server

---
+++ dns_entries_proxmox.tf
@@ -0,0 +1,23 @@
+resource "cloudflare_record" "internet-router-gw-proxmox-a-record" {
+  type    = "A"
+  ttl     = 60
+  zone_id = var.CLOUDFLARE_ZONE_ID
+  name    = "internet-router-gw.proxmox.${var.ROOT_DOMAIN_NAME}"
+  content = "172.16.0.2"
+}
+
+resource "cloudflare_record" "hotspotshield-vpn-gw-proxmox-a-record" {
+  type    = "A"
+  ttl     = 60
+  zone_id = var.CLOUDFLARE_ZONE_ID
+  name    = "hotspotshield-vpn-gw.proxmox.${var.ROOT_DOMAIN_NAME}"
+  content = "172.16.0.3"
+}
+
+resource "cloudflare_record" "zerotier-vpn-gw-proxmox-a-record" {
+  type    = "A"
+  ttl     = 60
+  zone_id = var.CLOUDFLARE_ZONE_ID
+  name    = "zerotier-vpn-gw.proxmox.${var.ROOT_DOMAIN_NAME}"
+  content = "172.16.0.4"
+}
