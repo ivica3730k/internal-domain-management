7cb95e01a45ae9e1ee96ccccd2c2e93ea1e586f6
Ivica Matić <ivica-matic@outlook.com>
2025-03-06 12:05:59 +0000

    Update dns_entries.tf

--- dns_entries.tf
+++ dns_entries.tf
@@ -5,3 +5,12 @@ resource "cloudflare_record" "localhost-a-record" {
   name    = "localhost.${var.ROOT_DOMAIN_NAME}"
   content = "127.0.0.1"
 }
+
+resource "cloudflare_record" "proxmox-a-record" {
+  type    = "A"
+  ttl     = 60
+  zone_id = var.CLOUDFLARE_ZONE_ID
+  name    = "proxmox.${var.ROOT_DOMAIN_NAME}"
+  content = "192.168.1.43"
+}
+
